67879f22aaf09ed6781a835ce1c0c3ec3048e89d
Ikbal Hossain Asif <asifikbalgfh312@gmail.com>
2023-09-13 01:01:44 +0600

    create errorMiddlewate

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ const express=require('express');
 const app=express();
 require('dotenv').config();
 const connectDB = require('./config/db');
-const userRouters=require('./routes/userRouter')
+const userRouters=require('./routes/userRouter');
+const { notFount, errorHandler } = require('./middleware/errorMiddleware');
 
 connectDB()
 app.use(express.json());
 
 app.use('/api/user',userRouters)
+
+app.use(notFount);
+app.use(errorHandler);
 
 app.get('/',(req,res)=>{
     res.send('chat app running')
